cd9b1d1aab2f45e4fe8dafb33f4474b8e4e1be56
Savan <sawanpatel2508@gmail.com>
2023-12-13 17:29:59 -0500

    Add FormAddFriend.jsx functionality.

--- src/App.js
+++ src/App.js
@@ -1,41 +1,52 @@
-import {FriendsList} from "./component/FriendsList";
-import {FormAddFriend} from "./component/FormAddFriend";
-import {Button} from "./component/Button";
-import {FormSplitBill} from "./component/FormSplitBill";
-import {useState} from "react";
+import { FriendsList } from "./component/FriendsList";
+import { FormAddFriend } from "./component/FormAddFriend";
+import { Button } from "./component/Button";
+import { FormSplitBill } from "./component/FormSplitBill";
+import { useState } from "react";
 
 const initialFriends = [
-    {
-        id: 118836,
-        name: "Clark",
-        image: "https://i.pravatar.cc/48?u=118836",
-        balance: -7,
-    },
-    {
-        id: 933372,
-        name: "Sarah",
-        image: "https://i.pravatar.cc/48?u=933372",
-        balance: 20,
-    },
-    {
-        id: 499476,
-        name: "Anthony",
-        image: "https://i.pravatar.cc/48?u=499476",
-        balance: 0,
-    },
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
 ];
 
 export default function App() {
-    const [showAddFriend, setShowAddFriend] = useState(false);
+  const [friends, setFriends] = useState(initialFriends);
+  const [showAddFriend, setShowAddFriend] = useState(false);
 
-    return (
-        <div className="app">
-            <div className="sidebar">
-                <FriendsList friends={initialFriends}/>
-                {showAddFriend && <FormAddFriend/>}
-                <Button onClick={}>Add Friend</Button>
-            </div>
-            <FormSplitBill/>
-        </div>
-    );
+  function handleShowFriend() {
+    setShowAddFriend((show) => !show);
+  }
+
+  function handleAddFriend(friend) {
+    setFriends((friends) => [...friends, friend]);
+  }
+
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <FriendsList friends={friends} />
+        {showAddFriend && <FormAddFriend onAddFriend={handleAddFriend} />}
+        <Button onClick={handleShowFriend}>
+          {showAddFriend ? "Close" : "Add Friend"}
+        </Button>
+      </div>
+      <FormSplitBill />
+    </div>
+  );
 }
--- src/component/FormAddFriend.jsx
+++ src/component/FormAddFriend.jsx
@@ -1,13 +1,45 @@
 import { Button } from "./Button";
+import { useState } from "react";
 
-export function FormAddFriend() {
+export function FormAddFriend({ onAddFriend }) {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("https://i.pravatar.cc/48");
+
+  function handleAddFriend(e) {
+    e.preventDefault();
+
+    const id = crypto.randomUUID();
+
+    if (!name || !image) return;
+
+    const newFriend = {
+      id,
+      name,
+      image: `${image}?=${id}`,
+      balance: 0,
+    };
+
+    onAddFriend(newFriend);
+
+    setName("");
+    setImage("https://i.pravatar.cc/48");
+  }
+
   return (
-    <form className="form-add-friend">
+    <form className="form-add-friend" onSubmit={handleAddFriend}>
       <label> Friend name</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
 
-      <label> Image URL</label>
-      <input type="text" />
+      <label> 🖼️ Image URL</label>
+      <input
+        type="text"
+        value={image}
+        onChange={(e) => setImage(e.target.value)}
+      />
 
       <Button>Add</Button>
     </form>
