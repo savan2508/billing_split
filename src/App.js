ba4044fa9c29135659805a0e7baac89b3053c350
Savan <sawanpatel2508@gmail.com>
2023-12-15 18:26:43 -0500

    Fix minor issue

--- src/App.js
+++ src/App.js
@@ -74,7 +74,11 @@ export default function App() {
         </Button>
       </div>
       {selectedFriend && (
-        <FormSplitBill friend={selectedFriend} onSplitBill={handleSplitBill} />
+        <FormSplitBill
+          friend={selectedFriend}
+          onSplitBill={handleSplitBill}
+          key={selectedFriend.id}
+        />
       )}
     </div>
   );
